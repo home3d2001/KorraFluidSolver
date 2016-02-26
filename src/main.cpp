3d048cbe3b6578a735ffd052decb7f9fd3e58f5f
Trung Le <trungle@seas.upenn.edu>
2016-02-26 13:35:08 -0500

    Fixed CmakeLists.txt typo to properly include OSX frameworks

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Make sure with don't use old profile
 
     // Open a window and create its OpenGL context
-    gWindow = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
+    gWindow = glfwCreateWindow( SCREEN_SIZE.x, SCREEN_SIZE.y, "Trungle Fluid", NULL, NULL);
     if( gWindow == NULL ){
         fprintf( stderr, "Failed to open GLFW window.\n" );
         getchar();
@@ -45,7 +45,35 @@ int main()
 
     glfwMakeContextCurrent(gWindow);
 
-    cout << "Hello world 2" << endl;
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(gWindow, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    do{
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Swap buffers
+        glfwSwapBuffers(gWindow);
+        glfwPollEvents();
+
+    }
+    // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(gWindow, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+           glfwWindowShouldClose(gWindow) == 0 );
+
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
 
